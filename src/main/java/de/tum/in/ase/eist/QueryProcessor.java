27ea19ccf24ee81660a199ff3b59b69f95f2457a
huiyu-yu- <huiyu.yu@tum.de>
2022-07-15 16:34:09 +0800

    21

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,6 +13,8 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "huiyu";
+           else if (query.contains("what is 6 plus 15")) {
+                return "21";
         } else { // TODO extend the programm here
             return "";
         }
